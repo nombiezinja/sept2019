609039421a0380e73bb5a7a5282d66cfca424641
Ti Zhang <tianyizhang1987@gmail.com>
2019-09-09 21:34:42 -0700

    parses file

--- script.js
+++ script.js
@@ -18,28 +18,70 @@ const readline = require('readline');
 const fs = require('fs');
 const stream = require('stream');
 
-const inStream = fs.createReadStream('./input.in')
+const inStream = fs.createReadStream('./input-large.in')
 const outStream = new stream();
 const rl = readline.createInterface(inStream, outStream);
 
 let lineNumber = 0;
+let testCaseNum = 1;
+let entry; 
+let exit;
 let beginTestCase;
 let endtestCase; 
 let nextTestCase;
+let coordinates;
+let testCases = [];
+let rawTestCase = [];
+
 rl.on('line', function(line) {
   // First line is the total number of test cases; array will have that number of objects
   lineNumber++;
   if (lineNumber === 1) {
-    console.log(`There are a total of ${line} test cases`);
     return;
   }
 
-  if (lineNumber === 2) {
+  if (lineNumber === 2 || lineNumber === nextTestCase) {
     beginTestCase = lineNumber;
+    N = Number(line.split(' ')[0]);
+    M = Number(line.split(' ')[1]);
+    coordinates = beginTestCase + 1;
+    endTestCase = coordinates + N
+    nextTestCase =  endTestCase + 1;
   }
 
-  console.log(line.split(' '));
-  endTestCase = Number(line.split(' ')[0]) + 1
-  nextTestCase = endTestCase + 1;
-  console.log(`TestCase begins at ${beginTestCase}, ends at${endTestCase}`);
+
+  if (lineNumber == coordinates) {
+    console.log(`Coordinates for test case is ${line}`);
+    entry = [Number(line.split(' ')[0]), Number(line.split(' ')[1])]  
+    exit = [Number(line.split(' ')[2]), Number(line.split(' ')[3])]  
+    return;
+  }
+
+  if (coordinates < lineNumber && lineNumber <= endTestCase) {
+    [...line.split(' ')].forEach((num)=>{
+      rawTestCase.push(num);
+    })
+  }
+
+  if (lineNumber == endTestCase) {
+
+    let testCase = {
+      testCase: testCaseNum, 
+      N: N, 
+      M: M, 
+      entry: entry, 
+      exit: exit, 
+      rawInput: rawTestCase
+    }
+    
+    rawTestCase = [];
+    testCases.push(testCase); 
+    testCaseNum++;
+  }
+  console.log("testCases are", testCases);
+
 });
+
+const parseLineContent = () => {
+
+}
